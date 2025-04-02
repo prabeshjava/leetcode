9ce9becbe9d3ac93dda107f8d5413d95ac525618
plinfotech123 <plinfotech123@gmail.com>
2025-04-01 23:27:11 -0500

    LeetCode Two Sum

---
+++ Array/TwoSum.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 4, 6, 8};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int compliment = target - nums[i];
+            if (numMap.containsKey(compliment)) {
+                return new int[]{numMap.get(compliment), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
